cdd5d3933a4585d2fed4a32a6c07cad2dfccf32d
Kamal Singh <kamal.singh@parason.com>
2024-03-18 04:53:33 +0000

    filter key activities

--- hrms_custom/public/js/appraisal_template.js
+++ hrms_custom/public/js/appraisal_template.js
@@ -1,3 +1,16 @@
+frappe.ui.form.on("Appraisal Template", {
+	refresh(frm, cdt, cdn) {
+		frm.set_query("custom_key_activity", "goals", function(doc, cdt, cdn) {
+			let d = locals[cdt][cdn];
+			return {
+				filters: [
+					['kra', '=', d.key_result_area]
+				]
+            }
+        })
+	}
+})
+
 frappe.ui.form.on("Appraisal Template Goal", {
 	key_result_area(frm, cdt, cdn) {
 		let d = frappe.get_doc(cdt, cdn);
