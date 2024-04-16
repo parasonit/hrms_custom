f1fe977d1c5dabd9f23a87bf522ac500f8fb1177
Kamal Singh <kamal.singh@parason.com>
2024-04-16 05:11:24 +0000

    update local changes

--- hrms_custom/hooks.py
+++ hrms_custom/hooks.py
@@ -161,9 +161,9 @@ doc_events = {
 		]
 	},
     "Appraisal Template": {
-        "validate": {
-            "hrms_custom.doc_events.auto_name_appr_temp"
-		},
+        # "validate": {
+        #     "hrms_custom.doc_events.auto_name_appr_temp"
+		# },
         "before_insert": [
             "hrms_custom.doc_events.update_employee",
             "hrms_custom.doc_events.update_reporting_manager"
