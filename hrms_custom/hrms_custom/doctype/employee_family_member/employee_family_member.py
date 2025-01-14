3449ea5832459c3cd43701cf213b9881a742c3c1
Kamal Singh <kamal.singh@parason.com>
2025-01-14 05:43:47 +0000

    update Employee Doctype

---
+++ hrms_custom/hrms_custom/doctype/employee_family_member/employee_family_member.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, 8848 and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class EmployeeFamilymember(Document):
+	pass
